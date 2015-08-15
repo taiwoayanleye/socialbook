cdeb2aa4472077a7d59cd6bd83b65fad3509e9ae
t <onlyyouandty@gmail.com>
2015-08-15 20:00:46 +0100

    complete newsfeed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked only: [:create], owner: Proc.new{ |controller, model| controller.current_user }
+
   belongs_to :user
   mount_uploader :attachment, AvatarUploader
 end
